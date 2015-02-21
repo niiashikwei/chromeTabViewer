8289ce74cb62c527dd7977e299562618f0d2e972
Nii <niiashikwei@hotmail.com>
2015-02-21 18:10:28 +0530

    Nii - getting tab title and icon objects

--- popup.js
+++ popup.js
@@ -1,1 +1,19 @@
-console.log("clicked!");
+
+tabs = [];
+
+queryInfo = {
+	"currentWindow": true
+};
+
+chrome.tabs.query(queryInfo , function(tabArray){
+	console.log(tabArray);
+	for(i = 0; i < tabArray.length; i++){
+		tab = {
+			"title": tabArray[i].title,
+			"favIconUrl": tabArray[i].favIconUrl
+		}
+		tabs.push(tab);
+	}
+});
+
+console.log(tabs);
