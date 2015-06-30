16a870f5247de94ff36b8ae593fe415a291962f1
Nii <niiashikwei@hotmail.com>
2015-06-30 10:12:37 +0000

    Nii - able to display tab names in tile view

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-tabsToDisplay = [];
 
-function getActiveTabs(){
+function getCurrentWindowTabs(){
     chrome.tabs.query({"currentWindow": true}, function (arrayOfTab) {
-        console.log("got array of Tabs");
         for (i = 0; i < arrayOfTab.length - 1; i++){
             var tab = {
                 "tabId": arrayOfTab[i].id,
                 "tabTitle": arrayOfTab[i].title,
                 "icon": arrayOfTab[i].favIconUrl
             };
-            tabsToDisplay.push(tab);
-            console.log(tab);
+
+            if (i % 3 == 0){
+                row = document.createElement('div');
+                row.setAttribute('class', 'row');
+            }
+
+            tile = document.createElement('div');
+            tile.setAttribute('class', 'tile');
+            tile.innerText = tab.tabTitle;
+
+            row.appendChild(tile);
+            document.getElementById("container").appendChild(row);
         }
+        console.log("got array of Tabs");
     });
 }
 
-getActiveTabs();
-console.log(tabsToDisplay);
+function saveTabCollection(){
+    console.log("coming soon!");
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    getCurrentWindowTabs();
+});
