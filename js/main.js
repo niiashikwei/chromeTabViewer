1685a8a1930414f6d96da97818d5a24858268afd
Nii <niiashikwei@hotmail.com>
2015-06-30 12:56:21 +0000

    Nii - on click listener added for switching to tab

--- js/main.js
+++ js/main.js
@@ -1,38 +1,44 @@
 
 function getCurrentWindowTabs(){
     chrome.tabs.query({"currentWindow": true}, function (arrayOfTab) {
-        for (i = 0; i < arrayOfTab.length - 2; i++){
-            var tab = {
-                "tabId": arrayOfTab[i].id,
-                "tabTitle": arrayOfTab[i].title,
-                "icon": arrayOfTab[i].favIconUrl
-            };
+        for (i = 0; i < arrayOfTab.length - 2; i++) {
+            (function () {
+                var tab = {
+                    "tabId": arrayOfTab[i].id,
+                    "tabTitle": arrayOfTab[i].title,
+                    "icon": arrayOfTab[i].favIconUrl
+                };
 
-            if (i % 3 == 0){
-                row = document.createElement('div');
-                row.setAttribute('class', 'row');
-            }
+                if (i % 3 == 0) {
+                    row = document.createElement('div');
+                    row.setAttribute('class', 'row');
+                }
 
-            tile = document.createElement('div');
-            tile.setAttribute('class', 'tile');
+                tile = document.createElement('div');
+                tile.setAttribute('class', 'tile');
+                tile.addEventListener('click', function () {
+                    chrome.tabs.update(tab.tabId, {selected: true}, function () {
+                        console.log("updating selected tab");
+                    });
+                });
 
-            img = document.createElement('img');
-            img.setAttribute('class', 'favIcon');
-            img.setAttribute('src', tab.icon);
-            img.setAttribute('width', '40px');
-            img.setAttribute('height', '40px');
+                img = document.createElement('img');
+                img.setAttribute('class', 'favIcon');
+                img.setAttribute('src', tab.icon);
+                img.setAttribute('width', '40px');
+                img.setAttribute('height', '40px');
 
-            h3 = document.createElement('h3');
-            h3.innerText = tab.tabTitle;
+                h3 = document.createElement('h3');
+                h3.innerText = tab.tabTitle;
 
-            tile.appendChild(h3);
-            tile.appendChild(img);
-            row.appendChild(tile);
-            document.getElementById("container").appendChild(row);
-        }
-        console.log("got array of Tabs");
-    });
-}
+                tile.appendChild(h3);
+                tile.appendChild(img);
+                row.appendChild(tile);
+                document.getElementById("container").appendChild(row);
+            }());
+            console.log("got array of Tabs");
+        }});
+    }
 
 function saveTabCollection(){
     console.log("coming soon!");
