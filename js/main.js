e1fbaf8b1c5e21372e19e8e783bf9f2fba60ad02
Nii <niiashikwei@hotmail.com>
2015-07-09 01:00:14 +0000

    Nii - minor refactor to extract tasks executed on each tab

--- js/main.js
+++ js/main.js
@@ -1,59 +1,54 @@
+function executeTasks(tab, tabViewerTabId) {
+    var tabObject = {
+        "tabId": tab.id,
+        "tabTitle": tab.title,
+        "icon": tab.favIconUrl
+    };
 
+    return function () {
+        if (tab.index % 3 == 0) {
+            row = document.createElement('div');
+            row.setAttribute('class', 'row');
+        }
+
+        tile = document.createElement('div');
+        tile.setAttribute('class', 'tile');
+        tile.addEventListener('click', function () {
+            //switch to selected tab
+            chrome.tabs.update(tabObject.tabId, {selected: true}, function () {
+                console.log("switching to selected tab");
+            });
+
+            chrome.tabs.remove(tabViewerTabId, function () {
+                console.log("removing extension page");
+            });
+        });
+
+        h3 = document.createElement('h3');
+        h3.innerText = tabObject.tabTitle;
+        tile.appendChild(h3);
+
+        if (tabObject.icon != undefined) {
+            img = document.createElement('img');
+            img.setAttribute('class', 'favIcon');
+            img.setAttribute('src', tabObject.icon);
+            img.setAttribute('width', '40px');
+            img.setAttribute('height', '40px');
+            tile.appendChild(img);
+        }
+
+        row.appendChild(tile);
+        document.getElementById("container").appendChild(row);
+    }();
+}
 function loadCurrentWindowTabs(){
     chrome.tabs.query({"currentWindow": true}, function (arrayOfTab) {
-        for (i = 0; i < arrayOfTab.length - 1; i++) {
-            (function () {
-                var tab = {
-                    "tabId": arrayOfTab[i].id,
-                    "tabTitle": arrayOfTab[i].title,
-                    "icon": arrayOfTab[i].favIconUrl
-                };
-
-                if (i % 3 == 0) {
-                    row = document.createElement('div');
-                    row.setAttribute('class', 'row');
-                }
-
-                tile = document.createElement('div');
-                tile.setAttribute('class', 'tile');
-                tile.addEventListener('click', function () {
-                    //switch to selected tab
-                    chrome.tabs.update(tab.tabId, {selected: true}, function () {
-                        console.log("switching to selected tab");
-                    });
-                    chrome.tabs.remove(arrayOfTab[arrayOfTab.length - 1].id, function(){
-                        console.log("removing extension page");
-                    });
-                });
-
-                h3 = document.createElement('h3');
-                h3.innerText = tab.tabTitle;
-                tile.appendChild(h3);
-
-                if(tab.icon != undefined){
-                    img = document.createElement('img');
-                    img.setAttribute('class', 'favIcon');
-                    img.setAttribute('src', tab.icon);
-                    img.setAttribute('width', '40px');
-                    img.setAttribute('height', '40px');
-                    tile.appendChild(img);
-                }
-
-                row.appendChild(tile);
-                document.getElementById("container").appendChild(row);
-            }());
-
+        var tabViewerId = arrayOfTab[arrayOfTab.length - 1].id;
+        for (i = 0; i < tabViewerId; i++) {
+            executeTasks(arrayOfTab[i], tabViewerId);
         }
         console.log("done");
     });
-}
-
-function saveTabCollection(){
-    console.log("coming soon!");
-}
-
-function closeTabFromTile(){
-    console.log("coming soon!");
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
